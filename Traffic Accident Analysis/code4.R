bd7c945e43cbad437c7a073271e8cdab30004d64
kg11 <33463334+kg11@users.noreply.github.com>
2018-07-15 22:00:55 +0530

    Add files via upload

---
+++ Traffic Accident Analysis/code4.R
@@ -0,0 +1,36 @@
+install.packages("caTools")
+install.packages("rpart")
+install.packages("MASS")
+install.packages("caTools")
+library(MASS)
+library(party)
+library(caTools)
+set.seed(2)
+data <- read.csv("final.csv")
+View(data)
+str(data)
+apply(data,2,function(x) round(length(unique(x))/nrow(data),3)*100)
+cols<-c("Day_of_Week","Accident_Severity","Number_of_Vehicles","Number_of_Casualties","Road_Type","Speed_limit","Pedestrian_Crossing.Human_Control","Light_Conditions","Weather_Conditions","Road_Surface_Conditions","Urban_or_Rural_Area","Casualty_Class","Sex_of_Casualty","Casualty_Severity","Vehicle_Type","Sex_of_Driver")
+for(i in cols)
+{
+  data[,i]=as.factor(data[,i])
+}
+summary(data)
+
+ind<-sample.split(Y=data$Urban_or_Rural_Area,SplitRatio = 0.8)
+traindf<-data[ind,]
+testdf<-data[!ind,]
+
+pct<-ctree(Urban_or_Rural_Area ~ .,data=traindf,controls=ctree_control(mincriterion=0.99,minsplit=200000))
+plot(pct)
+
+pwc<-predict(pct,testdf)
+pwc
+t<-table(predictions=pwc,actual=testdf$Urban_or_Rural_Area)
+t
+sum(diag(t))/sum(t)
+
+
+
+
+
